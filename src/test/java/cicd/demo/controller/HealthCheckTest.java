08c17f1f1028bbc0c5d64297d949c2aa79b784c6
JeongHoon Lee <sherry0_@kakao.com>
2024-06-15 22:05:32 +0900

    CI test-success & artifact test

--- src/test/java/cicd/demo/controller/HealthCheckTest.java
+++ src/test/java/cicd/demo/controller/HealthCheckTest.java
@@ -7,9 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HealthCheckTest {
 
-    @Test
-    void failTest() {
-        Assertions.assertThat(true).isEqualTo(false);
-    }
+//    @Test
+//    void FailTest() {
+//        Assertions.assertThat(true).isEqualTo(false);
+//    }
 
+    @Test
+    void SuccessTest() {
+        Assertions.assertThat(true).isEqualTo(true);
+    }
 }
