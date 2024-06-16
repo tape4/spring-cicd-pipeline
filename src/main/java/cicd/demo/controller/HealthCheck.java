a77b4e5451963f9898e474581ed07b2fda81ee0d
JeongHoon Lee <sherry0_@kakao.com>
2024-06-16 18:58:22 +0900

    healthCheck returns static UUID

--- src/main/java/cicd/demo/controller/HealthCheck.java
+++ src/main/java/cicd/demo/controller/HealthCheck.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 @Controller
 public class HealthCheck {
 
+    public static String uuid = UUID.randomUUID().toString();
     @ResponseBody
     @GetMapping("/health-check")
     public String health_check() {
-        return "OK";
+        return uuid;
     }
 }
